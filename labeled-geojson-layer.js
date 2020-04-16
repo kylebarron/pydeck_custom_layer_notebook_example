c5b4abb321e64db4f40f762ff0694e16881ab66b
Kyle Barron <barronk@mit.edu>
2020-04-16 14:47:11 -0600

    Run prettier

--- labeled-geojson-layer.js
+++ labeled-geojson-layer.js
@@ -1,14 +1,14 @@
-import centerOfMass from "@turf/center-of-mass";
-import { polygon } from "@turf/helpers";
-import area from "@turf/area";
-import { CompositeLayer } from "@deck.gl/core";
-import { GeoJsonLayer, TextLayer } from "@deck.gl/layers";
+import centerOfMass from "@turf/center-of-mass"
+import { polygon } from "@turf/helpers"
+import area from "@turf/area"
+import { CompositeLayer } from "@deck.gl/core"
+import { GeoJsonLayer, TextLayer } from "@deck.gl/layers"
 
 const defaultProps = {
   // Inherit all of GeoJsonLayer's props
   ...GeoJsonLayer.defaultProps,
   // Label for each feature
-  getLabel: { type: "accessor", value: x => x.text },
+  getLabel: { type: "accessor", value: (x) => x.text },
   // Label size for each feature
   getLabelSize: { type: "accessor", value: 32 },
   // Label color for each feature
@@ -20,42 +20,43 @@ const defaultProps = {
   // Label background color
   labelBackground: { type: "color", value: null, optional: true },
   // Label font
-  fontFamily: "Monaco, monospace"
-};
+  fontFamily: "Monaco, monospace",
+}
 
 function getLabelAnchors(feature) {
-  const {type, coordinates} = feature.geometry;
+  const { type, coordinates } = feature.geometry
   switch (type) {
-    case 'Point':
-      return [coordinates];
-    case 'MultiPoint':
-      return coordinates;
-    case 'Polygon':
-      return [centerOfMass(feature).geometry.coordinates];
-    case 'MultiPolygon':
-      let polygons = coordinates.map(rings => polygon(rings));
-      const areas = polygons.map(area);
-      const maxArea = Math.max.apply(null, areas);
+    case "Point":
+      return [coordinates]
+    case "MultiPoint":
+      return coordinates
+    case "Polygon":
+      return [centerOfMass(feature).geometry.coordinates]
+    case "MultiPolygon":
+      let polygons = coordinates.map((rings) => polygon(rings))
+      const areas = polygons.map(area)
+      const maxArea = Math.max.apply(null, areas)
       // Filter out the areas that are too small
-      return polygons.filter((f, index) => areas[index] > maxArea * 0.5)
-        .map(f => centerOfMass(f).geometry.coordinates);
+      return polygons
+        .filter((f, index) => areas[index] > maxArea * 0.5)
+        .map((f) => centerOfMass(f).geometry.coordinates)
     default:
-      return [];
+      return []
   }
 }
 
 class LabeledGeoJsonLayer extends CompositeLayer {
   updateState({ changeFlags }) {
-    const { data } = this.props;
+    const { data } = this.props
     if (changeFlags.dataChanged && data) {
       const labelData = (data.features || data).flatMap((feature, index) => {
-        const labelAnchors = getLabelAnchors(feature);
-        return labelAnchors.map(p =>
+        const labelAnchors = getLabelAnchors(feature)
+        return labelAnchors.map((p) =>
           this.getSubLayerRow({ position: p }, feature, index)
-        );
-      });
+        )
+      })
 
-      this.setState({ labelData });
+      this.setState({ labelData })
     }
   }
   renderLayers() {
@@ -66,27 +67,27 @@ class LabeledGeoJsonLayer extends CompositeLayer {
       labelSizeUnits,
       labelBackground,
       billboard,
-      fontFamily
-    } = this.props;
+      fontFamily,
+    } = this.props
     return [
       new GeoJsonLayer(this.props, this.getSubLayerProps({ id: "geojson" }), {
-        data: this.props.data
+        data: this.props.data,
       }),
       new TextLayer(this.getSubLayerProps({ id: "text" }), {
         data: this.state.labelData,
         billboard,
         sizeUnits: labelSizeUnits,
         backgroundColor: labelBackground,
-        getPosition: d => d.position,
+        getPosition: (d) => d.position,
         getText: this.getSubLayerAccessor(getLabel),
         getSize: this.getSubLayerAccessor(getLabelSize),
-        getColor: this.getSubLayerAccessor(getLabelColor)
-      })
-    ];
+        getColor: this.getSubLayerAccessor(getLabelColor),
+      }),
+    ]
   }
 }
 
-LabeledGeoJsonLayer.layerName = "LabeledGeoJsonLayer";
-LabeledGeoJsonLayer.defaultProps = defaultProps;
+LabeledGeoJsonLayer.layerName = "LabeledGeoJsonLayer"
+LabeledGeoJsonLayer.defaultProps = defaultProps
 
-export { LabeledGeoJsonLayer };
+export { LabeledGeoJsonLayer }
